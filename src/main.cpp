97f777cc5570299729e2bec00c215a0a8a1bf413
Daniel Graungaard <graungaard@cs,au.dk>
2021-04-03 14:37:30 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <IRremote.h>
+int IR_RECEIVE_PIN = 11; // Receive on pin 11
+
+void setup()
+{
+  Serial.begin(9600);
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); // Start the receiver, enable feedback LED, take LED feedback pin from the internal boards definition
+
+}
+
+void loop() {
+  if (IrReceiver.decode()) {
+    //IrReceiver.printIRResultShort(&Serial);
+    Serial.println(IrReceiver.decodedIRData.command);
+    IrReceiver.resume(); // Continue receiving
+  }
+}
